9da5b36b1f44f15a5fc29abca8095948c32649ca
Callum Wallace <36588838+CalWal@users.noreply.github.com>
2019-02-18 17:38:25 +1000

    Added projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,17 @@ function Projects() {
       <h1 className="projects">
         <span>PROJECTS</span>
       </h1>
+      <div className="projectCubes">
+        <h2 className="projectTitle">Pomodoro Clock</h2>
+        <p>A pomodoro clock made using React!</p>
+
+        <div>
+          <a href="">View On Github</a>
+        </div>
+        <div>
+          <a href="">Live</a>
+        </div>
+      </div>
     </div>
   );
 }
